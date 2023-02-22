40a472ee1c20bf07556c607c99ec2f6015972747
Jason <ya850805@gmail.com>
2023-02-22 18:06:08 +0800

    feat: Remove unused component then add map-dice basic template.

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,161 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1>Register</h1>
+      username: <input type="text" v-model="registerUsername" />
+      password: <input type="password" v-model="registerPassword" />
+      <button @click="register()">register</button>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <hr>
+
+      <h1>Login</h1>
+      username: <input type="text" v-model="username" :disabled="isLogin" />
+      password: <input type="password" v-model="password" :disabled="isLogin"/>
+      <button @click="login()" :disabled="isLogin">login</button>
+      <button @click="logout()" :disabled="!isLogin">logout</button>
+      <br>
+
+      <hr>
+
+      <h1>Dice</h1>
+      Place type:
+      <select v-model="type" :disabled="!isLogin">
+        <option value="restaurant">restaurant</option>
+      </select>
+      <button @click="dice()" :disabled="!isLogin">Dice</button>
+      <br>
+
+      <hr>
+
+      <div v-if="isLogin">
+        name : {{placeName}} <br>
+        rating: {{placeRating}} <br>
+        userRatingsTotal : {{placeUserRatingsTotal}} <br>
+        vicinity : {{placeVicinity}} <br>
+      </div>
     </div>
   </header>
-
-  <RouterView />
 </template>
 
+<script setup lang="ts">
+import {ref} from "vue";
+import axios from "axios";
+import {BACKEND_URL, RADIUS} from "@/constant/MapDiceConstant";
+
+let registerUsername = ref("")
+let registerPassword = ref("")
+
+let username = ref("")
+let password = ref("")
+let isLogin = ref(false)
+
+let type = ref("")
+let placeName = ref("")
+let placeRating = ref(0)
+let placeUserRatingsTotal = ref(0)
+let placeVicinity = ref("")
+
+function dice() {
+  if(type.value === "") {
+    alert('Please choose one type.')
+  } else {
+    getLocationThenDice()
+  }
+}
+
+function getLocationThenDice() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getPositionAndDice);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+function getPositionAndDice(position: { coords: { latitude: any; longitude: any; }; }) {
+  const latitude = position.coords.latitude
+  const longitude = position.coords.longitude
+  console.log(`latitude=${latitude} longitude=${longitude}`);
+
+  axios.get(`${BACKEND_URL}/dice?latitude=${latitude}&longitude=${longitude}&radius=${RADIUS}&type=${type.value}`,
+      {
+        auth : {
+          username : username.value,
+          password : password.value
+        }
+      }
+  )
+  .then(res => {
+    console.log(res)
+    if(res.data.code == 200) {
+      const data = res.data.data
+      placeName.value = data.name
+      placeRating.value = data.rating
+      placeUserRatingsTotal.value = data.userRatingsTotal
+      placeVicinity.value = data.vicinity
+    } else {
+      alert(res.data.data)
+    }
+  })
+  .catch(err => {
+    if(err.response.status == 401) {
+      alert("login required...")
+    }
+  })
+}
+
+function register() {
+  const registerUser = {
+    name : registerUsername.value,
+    password : registerPassword.value
+  }
+  axios.post(`${BACKEND_URL}/users/create`, registerUser)
+      .then(res => {
+        if(res.data.code == 200 && res.data.data == 1) {
+          alert("successfully create user")
+          registerUsername.value = ""
+          registerPassword.value = ""
+        } else {
+          alert(res.data.data)
+        }
+      })
+}
+
+function login() {
+  if (username.value == "") {
+    alert("username is required")
+    return
+  }
+
+  if (password.value == "") {
+    alert("password is required")
+    return
+  }
+
+  if(!isLogin) {
+    alert("please login first")
+    return
+  }
+
+  axios.post(`${BACKEND_URL}/login?username=${username.value}&password=${password.value}`)
+      .then(res => {
+        alert(res.data.data)
+        if(res.data.code === 200) {
+          isLogin.value = true
+        } else {
+          username.value = ""
+          password.value = ""
+          isLogin.value = false
+        }
+      })
+}
+
+function logout() {
+  isLogin.value = false
+  username.value = ""
+  password.value = ""
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/constant/MapDiceConstant.ts
@@ -0,0 +1,3 @@
+export const BACKEND_URL = "http://localhost:8001/map-dice"
+
+export const RADIUS = 1000
