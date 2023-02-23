c5b9d25f0a64d8f83f5e790797225527e5dc9b0e
Jason <ya850805@gmail.com>
2023-02-23 14:50:53 +0800

    feat: Add Dockerfile.

--- src/constant/MapDiceConstant.ts
+++ src/constant/MapDiceConstant.ts
@@ -1,3 +1,4 @@
-export const BACKEND_URL = "http://localhost:8001/map-dice"
+export const BACKEND_URL = "http://34.80.103.69:30316/map-dice"
+// export const BACKEND_URL = "http://map-dice:8001/map-dice"
 
 export const RADIUS = 1000
