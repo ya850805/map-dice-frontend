93c8d471c9210ed91783231873603d3bacd224ad
Jason <ya850805@gmail.com>
2023-03-27 00:01:21 +0800

    feat: Add forgot password component.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
+import ForgotPassword from '../views/ForgotPassword.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/register",
       name : 'register',
       component: Register
+    },
+    {
+      path: "/forgot-password/:uuid",
+      name: 'forgotPassword',
+      component: ForgotPassword
     }
   ]
 })
---
+++ src/views/ForgotPassword.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex-col w-100">
+    <p>{{ $t('_resetPassword') }}</p>
+    <div class="flex-row-sb pos-relative">
+      <input class="w-100" v-if="showPassword" type="text" v-model="password"/>
+      <input type="password" v-else class="w-100" v-model="password" />
+      <i class="i-password" @click="showPassword=!showPassword"></i>
+    </div>
+
+  </div>
+  <div class="flex-col w-100">
+    <p>{{ $t('_confirmResetPassword') }}</p>
+    <div class="flex-row-sb pos-relative">
+      <input class="w-100" v-if="showConfirmPassword" type="text" v-model="confirmPassword"/>
+      <input type="password" v-else class="w-100" v-model="confirmPassword" />
+      <i class="i-password" @click="showConfirmPassword=!showConfirmPassword"></i>
+    </div>
+  </div>
+
+  <div class="flex-col w-100 mt-40">
+    <button @click="resetPassword()">{{ $t('_resetPassword') }}</button>
+  </div>
+</template>
+
+<script setup>
+import {useRoute} from "vue-router/dist/vue-router";
+import {ref} from "vue";
+
+const route = useRoute()
+const uuid = route.params.uuid
+
+let password = ref("")
+let confirmPassword = ref("")
+let showPassword = ref(false)
+let showConfirmPassword = ref(false)
+
+function resetPassword() {
+  console.log(uuid)
+  console.log(password.value)
+  console.log(confirmPassword.value)
+  //TODO send request
+}
+
+</script>
+
+<style scoped>
+
+</style>
