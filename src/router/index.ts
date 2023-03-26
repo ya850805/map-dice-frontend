00da3d24fffdedc7c13e8c2d0113b377cae87dee
Jason <ya850805@gmail.com>
2023-03-27 00:07:58 +0800

    fix: Add default page as login page.

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      redirect: "/login"
+    },
+    {
       path: "/login",
       name : 'login',
       component: Login
