0b73e289c62593e3d25ee64b4b1d4314e4516f1b
Jason <ya850805@gmail.com>
2023-04-26 00:11:22 +0800

    feat: show collected places by vue props.

--- src/views/Collection.vue
+++ src/views/Collection.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="pop_content">
-    {{ collectPlaces }}
+    <div v-for="place in collectPlaces">
+      <a :href="place.url" target="_blank">{{ place.name }}</a><br>
+      網站：{{ place.website }}<br>
+      地址：{{ place.address }}
+      <hr>
+    </div>
   </div>
 </template>
 
