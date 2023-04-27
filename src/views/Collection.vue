35189953025410831a9d3d27fe86ae0646908821
Jason <ya850805@gmail.com>
2023-04-28 00:04:50 +0800

    feat: Add remove collection button.

--- src/views/Collection.vue
+++ src/views/Collection.vue
@@ -4,6 +4,7 @@
       <a :href="place.url" target="_blank">{{ place.name }}</a><br>
       網站：{{ place.website }}<br>
       地址：{{ place.address }}
+      <button>{{ $t('_removeCollect') }}</button>
       <hr>
     </div>
   </div>
